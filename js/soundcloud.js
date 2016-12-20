32db8b483e7113de4a184c96409fe852929409a5
Jon Holmberg <Jon Holmberg>
2016-12-20 00:12:39 -0800

    sound test

--- js/soundcloud.js
+++ js/soundcloud.js
@@ -1,5 +1,8 @@
-import React from 'react';
-import ReactDOM  from 'react-dom';
+// import React from 'react';
+// import ReactDOM  from 'react-dom';
+
+var React = require('react');
+var ReactDOM = require('react-dom');
 
 function SoundCloudEmbed(props) {
     const playerUrl = 'https://w.soundcloud.com/player/';
