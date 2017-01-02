7f0c862bee052f3b38e67874e98f1d7d22c33097
Jon Holmberg <Jon Holmberg>
2017-01-01 17:44:55 -0800

    finish board component

--- js/components/board.js
+++ js/components/board.js
@@ -1,31 +1,28 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var List = require('./list');
-var Card = require('./card');
-var ListContainer = require('./list-container');
+import ListContainer from './list-container';
 
 class Board extends React.Component {
       constructor(props) {
       super(props);
       this.state = {value: ''};
-
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
   }
 
 
 render(){
 
+    
 	return (
 	<div>
-		<ListContainer />
+        <ListContainer title = "to do" />
+        <ListContainer  title = "progress"/>
+        <ListContainer title="done"/>
 	</div>
 		);
 }//end render
     
     
-} // end board
+}
 
-module.exports = Board;
+export default Board;
 
-//Next make the Board component that will render three lists to view progress and finished state
